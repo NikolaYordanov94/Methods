dd4dd3c0213ac5830264124a706f6bcda2c320f1
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-09-03 00:58:26 +0300

    Add files via upload

---
+++ CalculateRectangleArea_06.java
@@ -0,0 +1,21 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class CalculateRectangleArea_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double width = Double.parseDouble(scanner.nextLine());
+        double length = Double.parseDouble(scanner.nextLine());
+
+        double area = calculateRectangleArea(width, length);
+
+        System.out.printf("%.0f", area);
+    }
+
+    public static double calculateRectangleArea (double width, double length){
+
+        return width * length;
+    }
+}
